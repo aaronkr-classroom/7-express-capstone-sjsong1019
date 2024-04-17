8cfff50efb1adbd1c340a2c439a6544f9bde04fa
sjsong1019 <163220368+sjsong1019@users.noreply.github.com>
2024-04-17 02:47:42 +0000

    7주

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,19 +1,32 @@
 // errorController.js
 "use strict";
 
-
+const httpStatus = require("http-status-codes");
 
 /**
  * Listing 11.2 (p. 168)
  */
-
+exports.logErrors = (err, req, res, next) => {
+    console.error(err.stack);
+    next(err);
+}; // @TODO: 에러 처리를 위한 미들웨어 추가
 
 /**
  * Listing 12.11 (p. 184)
  * 에러 처리 라우트 추가
  */
+exports.pageNotFoundError = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND; // 404
+    res.status(errorCode);
+    res.render("error");
+}
 
-
+exports.internalServerError = (err, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR; // 500
+    console.log(`ERROR occurred: ${errorCode.stack}`);
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+};
 /**
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
  * 기존 라우트는 범용으로 사용되는 것이며 하위 라우트에 오버라이딩되기 때문이다.
--- main.js
+++ main.js
@@ -2,30 +2,50 @@
 // Capstone 2: Express
 "use strict";
 
+const express = require("express");
 // 앱 설정
+const app = express();
+const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
+
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
+const layouts = require("express-ejs-layouts");
 
+app.set("view engine", "ejs");
+app.use(layouts);
 
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
-
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
-
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedContactForm);
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
-
+app.use(errorController.pageNotFoundError);
+// app.use(errorController.internalServerError);
 
 // 3000번 포트로 리스닝 설정
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+});
